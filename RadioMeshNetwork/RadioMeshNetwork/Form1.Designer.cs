cfff442734b3a24b0c4bd1f155226563eedffcde
Jake Mauk <larcondos@yahoo.com>
2019-02-23 09:51:07 -0500

    Updated the form with additional buttons

--- RadioMeshNetwork/RadioMeshNetwork/Form1.Designer.cs
+++ RadioMeshNetwork/RadioMeshNetwork/Form1.Designer.cs
@@ -35,6 +35,10 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.TypeMessageTextbox = new System.Windows.Forms.TextBox();
             this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SendMessageButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // MessagesListBox
@@ -78,11 +82,51 @@
             this.TypeMessageTextbox.TabIndex = 4;
             this.TypeMessageTextbox.Text = "Type your message here...";
             // 
+            // SendMessageButton
+            // 
+            this.SendMessageButton.Location = new System.Drawing.Point(270, 461);
+            this.SendMessageButton.Name = "SendMessageButton";
+            this.SendMessageButton.Size = new System.Drawing.Size(318, 50);
+            this.SendMessageButton.TabIndex = 5;
+            this.SendMessageButton.Text = "Click here to send your message!";
+            this.SendMessageButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Connect to Network";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Disconnect from Network";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 474);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 37);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Network Test";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 552);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SendMessageButton);
             this.Controls.Add(this.TypeMessageTextbox);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.ConfirmationTextbox);
@@ -103,6 +147,10 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.TextBox TypeMessageTextbox;
         private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button SendMessageButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
