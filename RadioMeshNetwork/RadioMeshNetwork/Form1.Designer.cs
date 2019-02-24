a878ae077e555e74c081612f42484a0d4fb7d533
Jake Mauk <larcondos@yahoo.com>
2019-02-23 19:47:23 -0500

    ports

--- RadioMeshNetwork/RadioMeshNetwork/Form1.Designer.cs
+++ RadioMeshNetwork/RadioMeshNetwork/Form1.Designer.cs
@@ -36,9 +36,10 @@
             this.TypeMessageTextbox = new System.Windows.Forms.TextBox();
             this.serialPort = new System.IO.Ports.SerialPort(this.components);
             this.SendMessageButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.COMPortsCheckedListBox = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // MessagesListBox
@@ -54,7 +55,7 @@
             this.AvaialbleRadiosCheckedListBox.FormattingEnabled = true;
             this.AvaialbleRadiosCheckedListBox.Location = new System.Drawing.Point(13, 12);
             this.AvaialbleRadiosCheckedListBox.Name = "AvaialbleRadiosCheckedListBox";
-            this.AvaialbleRadiosCheckedListBox.Size = new System.Drawing.Size(251, 319);
+            this.AvaialbleRadiosCheckedListBox.Size = new System.Drawing.Size(251, 139);
             this.AvaialbleRadiosCheckedListBox.TabIndex = 1;
             // 
             // ConfirmationTextbox
@@ -90,24 +91,26 @@
             this.SendMessageButton.TabIndex = 5;
             this.SendMessageButton.Text = "Click here to send your message!";
             this.SendMessageButton.UseVisualStyleBackColor = true;
+            this.SendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
             // 
-            // button1
+            // connectButton
             // 
-            this.button1.Location = new System.Drawing.Point(13, 387);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(251, 37);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Connect to Network";
-            this.button1.UseVisualStyleBackColor = true;
+            this.connectButton.Location = new System.Drawing.Point(13, 387);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(251, 37);
+            this.connectButton.TabIndex = 6;
+            this.connectButton.Text = "Connect to Radio";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
-            // button2
+            // disconnectButton
             // 
-            this.button2.Location = new System.Drawing.Point(12, 430);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(251, 37);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Disconnect from Network";
-            this.button2.UseVisualStyleBackColor = true;
+            this.disconnectButton.Location = new System.Drawing.Point(12, 430);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(251, 37);
+            this.disconnectButton.TabIndex = 7;
+            this.disconnectButton.Text = "Disconnect from Radio";
+            this.disconnectButton.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -118,14 +121,23 @@
             this.button3.Text = "Network Test";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // COMPortsCheckedListBox
+            // 
+            this.COMPortsCheckedListBox.FormattingEnabled = true;
+            this.COMPortsCheckedListBox.Location = new System.Drawing.Point(13, 158);
+            this.COMPortsCheckedListBox.Name = "COMPortsCheckedListBox";
+            this.COMPortsCheckedListBox.Size = new System.Drawing.Size(250, 169);
+            this.COMPortsCheckedListBox.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 552);
+            this.Controls.Add(this.COMPortsCheckedListBox);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
             this.Controls.Add(this.SendMessageButton);
             this.Controls.Add(this.TypeMessageTextbox);
             this.Controls.Add(this.progressBar1);
@@ -134,6 +146,7 @@
             this.Controls.Add(this.MessagesListBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,9 +161,10 @@
         private System.Windows.Forms.TextBox TypeMessageTextbox;
         private System.IO.Ports.SerialPort serialPort;
         private System.Windows.Forms.Button SendMessageButton;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckedListBox COMPortsCheckedListBox;
     }
 }
 
--- RadioMeshNetwork/RadioMeshNetwork/Form1.cs
+++ RadioMeshNetwork/RadioMeshNetwork/Form1.cs
@@ -5,16 +5,53 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO.Ports;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace RadioMeshNetwork
 {
     public partial class Form1 : Form
     {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console.
+            foreach(string port in ports)
+            {
+                Console.WriteLine(port);
+                COMPortsCheckedListBox.Items.Add(port);
+            }
+
+
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void SendMessageButton_Click(object sender, EventArgs e)
+        {
+            // Write to the port
+            serialPort.Write(TypeMessageTextbox.Text);
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            // Open a serial comm port
+            serialPort.Open();
+            
+        }
+
+
     }
 }
